2957787dccd67010bcdef3f3d266cd780c4c3c6c
greg lenard <gregory.lenard@colorado.edu>
2021-02-13 15:16:37 -0700

    adding templates

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
-from flask import 
+from flask import Flask, render_template
+from flask import request
+app = Flask(__name__)
 
-Flaskapp = Flask(__name__)
 @app.route('/')
+def index():
+    return render_template('index.html')
 
-def index():    
-    return '<h1>Hello World!</h1>'
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
